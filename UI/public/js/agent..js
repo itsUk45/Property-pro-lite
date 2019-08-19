0cd7d20e160a219adb1d5d35152071717f8283bd
kose-uk45 <kosebilaliuk45@gmail.com>
2019-08-19 16:02:48 +0200

    feature(ui agent):build the admin dashboard for agent[Start #167949629]

---
+++ UI/public/js/agent..js
@@ -0,0 +1,11 @@
+const updateAd = () =>{
+			alert("updated ads");
+
+};
+const markAsSold = () =>{
+           alert("status successfuly changed to SOLD");
+};
+
+const deleteAd = () =>{
+ 			alert("Property ad successfuly deleted");
+};
